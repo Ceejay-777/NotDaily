6f74f6300e6fe66ae712e4fa031e1e0f5e32de9e
Ceejay-777 <covenantcrackslord01@gmail.com>
2024-10-08 08:00:15 +0100

    Begin work on header

---
+++ app/home/page.tsx
@@ -0,0 +1,12 @@
+import Header from '@/components/header'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className=''>
+        <Header/>
+    </div>
+  )
+}
+
+export default page
