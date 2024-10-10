9ce94477b880d222243bb8fa4692b56f61159940
Ceejay-777 <covenantcrackslord01@gmail.com>
2024-10-10 18:52:39 +0100

    I dont know what Im doing yet

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
+import Header from '@/components/header';
 import '../styles/globals.css';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
             <body>
+                <Header />
                 {children}
             </body>
         </html>
