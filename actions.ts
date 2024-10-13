9aafc118558102ebfff25ff8e98e840bbe8a2820
Ceejay-777 <covenantcrackslord01@gmail.com>
2024-10-13 02:26:21 +0100

    working on searcH

--- actions.ts
+++ actions.ts
@@ -1,16 +1,19 @@
 import axios from "axios";
 const key = process.env.NEXT_PUBLIC_ALPHA_API_KEY;
 
-export const searchStock = async (term: string) => {
+export const searchStock = async (query: string) => {
   console.log("Okay");
-  const url = `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${term}&apikey=${key}`;
+  // const url = `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${query}&apikey=${key}`;
+  const url = `https://finnhub.io/api/v1/search?q=${query}&token=cqoalv9r01qo8865rbb0cqoalv9r01qo8865rbbg`
 
-  if (term) {
+  if (query) {
     try {
       const response = await axios(url);
       const data = response.data;
-      return data
-      console.log(data);
+      // console.log(data);
+      const choice = data.count > 6 ? data.result.slice(0,6) : data.result
+      console.log(choice)
+      return choice
     } catch (error: any) {
       console.log(error.message);
       return error.message
--- components/searchResults.tsx
+++ components/searchResults.tsx
@@ -1,10 +1,30 @@
 import { searchStock } from '@/actions'
 import React from 'react'
 
-const SearchResults = async ({query} : {query : string}) => {
+type ResultType = {
+  description: string;
+  displaySymbol: string;
+  [key: string]: any;
+};
+
+const SearchResults = async ({ query }: { query: string }) => {
+  const results = await searchStock(query)
 
   return (
-    <div className='border p-2'>{query || "No query"}</div>
+    <div>
+      <div className='border p-2'>{query || "No query"}</div>
+      {/* {
+       results && results.map((result: ResultType) => {
+          const { description, displaySymbol } = result
+          return (
+            <>
+              <div>displaySymbol</div>
+              <div>description</div>
+            </>
+          )
+        })
+      } */}
+    </div>
   )
 }
 
