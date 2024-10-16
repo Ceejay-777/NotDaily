06715282704ea703179b9266da7eb09da64efd8e
Ceejay-777 <covenantcrackslord01@gmail.com>
2024-10-16 04:24:00 +0100

    commit

--- app/stockoverview/page.tsx
+++ app/stockoverview/page.tsx
@@ -1,8 +1,11 @@
+import Breadcrumbs from '@/components/breadcrumbs'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>Page</div>
+    <div>
+      <Breadcrumbs />
+    </div>
   )
 }
 
--- components/breadcrumbs.tsx
+++ components/breadcrumbs.tsx
@@ -1,12 +1,41 @@
+'use client'
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import React from 'react'
 
+interface BreadcrumbItem {
+  href: string;
+  label: string;
+  isCurrent: boolean;
+}
+
 const Breadcrumbs = () => {
+  const pathname = usePathname() || ""
+  const paths : string[] = pathname.split('/').filter((path) => path && path !== "home");
+
+  const breadcrumbs: BreadcrumbItem[] = paths.map((path, index) => {
+    const href = `/${paths.slice(0, index + 1).join('/')}`;
+    console.log(href)
+    return {
+      href,
+      label: path.charAt(0).toUpperCase() + path.slice(1),
+      isCurrent: index === paths.length - 1
+    };
+  });
+
   return (
     <div className='breadcrumbs'>
       <ul>
-        <li>One</li>
-        <li>Two</li>
-        <li>Three</li>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        {breadcrumbs.map((breadcrumb) => {
+          return (
+            <li key={breadcrumb.href}>
+              <Link href={breadcrumb.href}>{breadcrumb.label}</Link>
+            </li>
+          )
+        })}
       </ul>
     </div>
   )
